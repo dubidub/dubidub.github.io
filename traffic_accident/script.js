a4521c6d8b1839124b6689087eb9c5447e03b8b6
dubidub <dub.wu.dub@gmail.com>
2021-02-05 15:26:59 +0800

    Almost there

--- traffic_accident/script.js
+++ traffic_accident/script.js
@@ -14,3 +14,28 @@ window.onclick = function(event) {
     }
   }
 }
+
+function tp_maplink1() {
+  var map = document.getElementById('map');
+  map.src = "https://dubidub.github.io/traffic_accident/resources/hexbin_tp";
+}
+function tp_maplink2() {
+  var map = document.getElementById('map');
+  map.src = "https://dubidub.github.io/traffic_accident/resources/customizable_tp";
+}
+function tc_maplink1() {
+  var map = document.getElementById('map');
+  map.src = "https://dubidub.github.io/traffic_accident/resources/hexbin_motion_tc";
+}
+function tc_maplink2() {
+  var map = document.getElementById('map');
+  map.src = "https://dubidub.github.io/traffic_accident/resources/customizable_tc";
+}
+function kh_maplink1() {
+  var map = document.getElementById('map');
+  map.src = "https://dubidub.github.io/traffic_accident/resources/heatmap_motion_kh";
+}
+function kh_maplink2() {
+  var map = document.getElementById('map');
+  map.src = "https://dubidub.github.io/traffic_accident/resources/customizable_kh";
+}
