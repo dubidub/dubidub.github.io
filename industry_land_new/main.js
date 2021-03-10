47ea23deed98700cb847a7249d635ad012195539
dubidub <dub.wu.dub@gmail.com>
2021-03-10 15:23:45 +0800

    GIS update

---
+++ industry_land_new/main.js
@@ -0,0 +1,12 @@
+const loadCoord = [23.707398117586052, 120.98712417755992];
+const map = L.map('mapid', {
+  worldCopyJump: true,
+}).setView(loadCoord, 8);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+var datasets;
+$("#inputFile").change(function(e) {
+    onChange(e);
+});
+var uniqueID;
